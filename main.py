660a02f7590bc05b3e512ccc3a1fbdbd12652ff5
DEV_RA3D <168814487+DEV-Ra3d@users.noreply.github.com>
2025-10-21 11:01:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,12 @@ from pytgcalls import idle
 import os
 import sys
 import random
-import asyncio
 from pyrogram import Client
 from pytgcalls import PyTgCalls
-from bot import *
+from bot import start_zombiebot
 from pyromod import listen
 
-
-
+# بدء تشغيل البوت
 loop = asyncio.get_event_loop()
 loop.run_until_complete(start_zombiebot())
 
